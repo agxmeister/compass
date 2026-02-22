c83d30801e36dee226dac2b6af4ee95ee9564a64
agxmeister <agxmeister@gmail.com>
2026-02-22 11:59:49 +0200

    Remove unused HTTP service.

--- src/modules/http/index.ts
+++ src/modules/http/index.ts
@@ -1,11 +1,7 @@
 export { HttpClient } from './HttpClient.js';
 export { HttpClientFactory } from './HttpClientFactory.js';
-export { HttpService } from './HttpService.js';
-export { HttpServiceFactory } from './HttpServiceFactory.js';
 export type {
     HttpEndpoint,
     HttpClient as HttpClientInterface,
     HttpClientFactory as HttpClientFactoryInterface,
-    HttpService as HttpServiceInterface,
-    HttpServiceFactory as HttpServiceFactoryInterface,
 } from './types.js';
--- src/modules/http/types.ts
+++ src/modules/http/types.ts
@@ -1,5 +1,3 @@
-import { z as zod } from 'zod';
-
 export type HttpEndpoint = {
     method: string;
     path: string;
@@ -12,13 +10,4 @@ export interface HttpClient {
 
 export interface HttpClientFactory {
     create(baseUrl: string): HttpClient;
-}
-
-export interface HttpService {
-    requestJson<T extends Record<string, unknown>>(endpoint: HttpEndpoint, schema: zod.ZodType<T>, body?: Record<string, unknown>): Promise<T>;
-    requestBinary(endpoint: HttpEndpoint): Promise<ArrayBuffer>;
-}
-
-export interface HttpServiceFactory {
-    create(baseUrl: string): HttpService;
 }
